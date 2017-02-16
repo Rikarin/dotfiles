36658d52d6e02cce28c8e1623c23a5e12389191a
Satoshi <satoshi@rikarin.org>
2017-02-17 00:00:27 +0100

    Add

--- basic_stuff.sh
+++ basic_stuff.sh
@@ -1,11 +1,11 @@
 # GUI
-sudo pacman -S xorg xorg-server xorg-xinit xorg-server-utils xorg-apps nautilus leafpad lua ruby libpulse
-yaourt -S bspwm sxhkd tint2 dmenu conky-lua-archers
+#sudo pacman -S xorg xorg-server xorg-xinit xorg-server-utils xorg-apps nautilus leafpad lua ruby libpulse htop
+#yaourt -S bspwm sxhkd tint2 dmenu conky-lua-archers
 
 # Terminal
-#sudo pacman -S termite zsh powerline vim tmux ttf-dejavu
-#yaourt -S ttf-pragmatapro
-#chsh -s `which zsh`
+sudo pacman -S termite zsh powerline vim tmux ttf-dejavu
+yaourt -S ttf-pragmatapro
+chsh -s `which zsh`
 
 # Other
-sudo pacman -S chromium wget
+#sudo pacman -S chromium wget
---
+++ link.sh
@@ -0,0 +1,4 @@
+ln -sf ~/.dotfiles/.xinitrc ~/.xinitrc
+ln -sf ~/.dotfiles/.Xresources ~/.Xresources
+ln -sf ~/.dotfiles/.colors ~/.colors
+ln -sf ~/.dotfiles/.config ~/.config
