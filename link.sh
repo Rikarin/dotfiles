86f7f8752a9a1d217febf9decae25b7ebb20730e
Satoshi <satoshi@rikarin.org>
2017-02-17 12:37:17 +0100

    Add git

--- link.sh
+++ link.sh
@@ -1,3 +1,6 @@
 ln -sf ~/.dotfiles/xinitrc ~/.xinitrc
 ln -sf ~/.dotfiles/colors ~/.colors
 ln -sf ~/.dotfiles/config ~/.config
+ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/git/gitignore_global ~/.gitignore_global
+
